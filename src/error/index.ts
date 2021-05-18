9d971bb26524ff5a65fa96d21ee783ae8b46a90e
xmrhrx <mrhr12345@gmail.com>
2021-05-18 14:43:23 +0800

    chore: new&commented code for future code

--- src/error/index.ts
+++ src/error/index.ts
@@ -1,1 +1,9 @@
+// import { NextFunction } from 'express';
+// import { APIErrors } from './api';
+export * from './api';
 export * from './session';
+
+// export function APIErrorHandler(err: APIErrors, req: Request, res: Response, next: NextFunction): Response | void {
+
+
+// }
